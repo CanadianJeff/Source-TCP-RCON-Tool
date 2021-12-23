7bf2e2681fd262cb92d2c81cf6eb3b21ea2e94bb
Jeffery Wilkins <djcanadianjeff@gmail.com>
2021-12-22 18:55:35 -0800

    remove pointless crap

--- Source TCP RCON Tool/Form1.cs
+++ Source TCP RCON Tool/Form1.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using SimpleTCP;
-using System.Text.RegularExpressions;
 
 namespace Source_TCP_RCON_Tool
 {
